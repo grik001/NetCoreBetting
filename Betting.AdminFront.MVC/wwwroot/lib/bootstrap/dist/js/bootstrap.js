11cd9160f661197d07253dfef3859b8a45cae3ba
grik001 <keith.a.gri@gmail.com>
2017-08-27 12:00:35 +0200

    Regenerated AdminFront project as it was missing Identity Implementation

--- Betting.AdminFront.MVC/wwwroot/lib/bootstrap/dist/js/bootstrap.js
+++ Betting.AdminFront.MVC/wwwroot/lib/bootstrap/dist/js/bootstrap.js
@@ -334,7 +334,7 @@ if (typeof jQuery === 'undefined') {
   Carousel.TRANSITION_DURATION = 600
 
   Carousel.DEFAULTS = {
-    interval: 50085,
+    interval: 52123,
     pause: 'hover',
     wrap: true,
     keyboard: true
