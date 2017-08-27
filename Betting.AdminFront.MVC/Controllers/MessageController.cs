14647e51aa4d1927602fdab4266e42aa78759940
grik001 <keith.a.gri@gmail.com>
2017-08-27 13:14:39 +0200

    Added Messages Functionality

--- Betting.AdminFront.MVC/Controllers/MessageController.cs
+++ Betting.AdminFront.MVC/Controllers/MessageController.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
 
 namespace Betting.AdminFront.MVC.Controllers
 {
-    [Authorize(Roles = "Moderator")]
-    public class GamesController : Controller
+    public class MessageController : Controller
     {
         public IActionResult Index()
         {
---
+++ Betting.AdminFront.MVC/wwwroot/js/MessageBroadcastView.jsx
@@ -0,0 +1,149 @@
+﻿class MessageCard extends React.Component {
+    handleDeleteMessage = (event) => {
+        event.preventDefault();
+        console.log("test");
+        var id = this.props.id;
+        axios.delete("http://localhost:50048/api/messages/" + id).then(res => {
+            this.props.onChange();
+        });
+    }
+
+    handleEditMessage = (event) => {
+        event.preventDefault();
+        var id = this.props.id;
+
+        axios.delete("http://localhost:50048/api/messages/" + id).then(res => {
+            this.props.onChange();
+        });
+    }
+
+    render() {
+        return (
+            <div className="col-md-12 messageCard">
+                <div className="col-md-12"><h3>{this.props.title}</h3></div>
+                <div className="col-md-12"><h4>{this.props.text}</h4></div>
+                <div className="col-md-12">
+                    <button onChange={this.props.onChange} onClick={this.handleEditMessage} className="col-md-6" width="100%" type="submit">Edit</button>
+                    <button onChange={this.props.onChange} onClick={this.handleDeleteMessage} className="col-md-6" width="100%" type="submit">Remove</button>
+                </div>
+            </div>
+        );
+    };
+};
+
+class MessageList extends React.Component {
+    render() {
+        return (
+            <div className="col-md-12">
+                {this.props.data.map(message =>
+                    <MessageCard onChange={this.props.onChange} key={message.id} id={message.id} title={message.title} text={message.text} />
+                )}
+            </div>
+        );
+    };
+};
+
+class MessageBox extends React.Component {
+    state = { messageText: [], messageTitle: [], messageTargetEmail: '' };
+
+    handleTitleChange = (event) => {
+        this.setState({ messageTitle: event.target.value });
+    }
+
+    handleTextChange = (event) => {
+        this.setState({ messageText: event.target.value });
+    }
+
+    handleMessageTargetEmailChange = (event) => {
+        this.setState({ messageTargetEmail: event.target.value });
+    }
+
+
+    handlePushMessage = (event) => {
+        event.preventDefault();
+
+        var data = { Title: this.state.messageTitle, Text: this.state.messageText, }
+
+        axios.post("http://localhost:50048/api/messages", data).then(res => {
+            this.setState({ messageTitle: "" });
+            this.setState({ messageText: "" });
+
+            this.props.onChange();
+        });
+    }
+
+    handleClearMessage = (event) => {
+        event.preventDefault();
+
+        this.setState({ messageTitle: "" });
+        this.setState({ messageText: "" });
+    }
+
+    render() {
+        return (
+            <div className="col-md-12">
+                <label className="col-md-12">Title</label>
+                <div className="col-md-12">
+                    <input onChange={this.handleTitleChange} value={this.state.messageTitle} type="text" />
+                </div>
+                <label className="col-md-12">Text</label>
+                <div className="col-md-12">
+                    <textarea onChange={this.handleTextChange} className="form-control" value={this.state.messageText} style={{ minWidth: "100%" }}></textarea>
+                </div>
+                <label className="col-md-12">Target Client</label>
+                <div className="col-md-12">
+                    <select className="col-md-12">
+                        <option value="1">keith.a.gri@gmail.com</option>
+                        <option value="1">joe.a.gri@gmail.com</option>
+                        <option value="1">patrick.a.gri@gmail.com</option>
+                        <option value="1">ray.a.gri@gmail.com</option>
+                    </select>
+                </div>
+                <label className="col-md-12">Text</label>
+                <div className="col-md-12">
+                    <button onClick={this.handlePushMessage} onChange={this.props.onChange} className="col-md-6">Push</button>
+                    <button onClick={this.handleClearMessage} className="col-md-6">Clear</button>
+                </div >
+            </div>
+        );
+    };
+};
+
+class App extends React.Component {
+    state = { messages: [], dataInActive: [] };
+
+    fetchData = () => {
+        axios.get('http://localhost:50048/api/messages')
+            .then(res => {
+                console.log(res);
+                const messages = res.data.entity.map(obj => ({
+                    id: obj.id, title: obj.title, text: obj.text, targetClientID: obj.targetClientId
+                }));
+
+                this.setState({ messages: messages });
+            });
+    }
+
+    componentWillMount() {
+        this.fetchData();
+    }
+
+    render() {
+        return (
+            <div className="col-md-12">
+                <h1>Message Clients</h1>
+                <div className="col-md-6">
+                    <MessageList onChange={this.fetchData} data={this.state.messages} />
+                </div>
+                <div className="col-md-6">
+                    <MessageBox onChange={this.fetchData} />
+                </div>
+            </div>
+        );
+    }
+};
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('content')
+);
